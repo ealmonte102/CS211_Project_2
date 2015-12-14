d92f2ac6e949639b23a4971e56db37855006665d
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-13 23:19:59 -0500

    Adds the Utilities cpp and hpp file from previous assignments

---
+++ src/Utilities.cpp
@@ -0,0 +1,58 @@
+// Utilities.cpp
+// Created by Evan Almonte on 10/18/2015.
+//
+#include "Utilities.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+namespace Project2_Utilites {
+	void clearScreen ( ) {
+#ifdef _WIN32
+		std::system ("CLS");
+#elif _WIN64
+		std::system ("CLS");
+#else
+		std::system ("CLEAR");
+#endif
+	}
+
+	bool getYesOrNo ( ) {
+		string answer;
+		getline (cin, answer);
+		while (answer != "Y" && answer != "y" && answer != "N" && answer != "n" &&
+			   upperConvert (answer) != "YES" && upperConvert (answer) != "NO") {
+			getline (cin, answer);
+		}
+		switch (answer[0]) {
+			case 'y':
+			case 'Y':
+				return true;
+			case 'n':
+			case 'N':
+			default:
+				return false;
+		}
+	}
+
+	int getValidInt ( ) {
+		int numToReturn;
+		while (!(cin >> numToReturn)) {
+			cin.clear ( );
+			cin.ignore (100, '\n');
+		}
+		cin.ignore ( );
+		return numToReturn;
+	}
+
+	string upperConvert (string data) {
+		for (unsigned int i = 0; i < data.length ( ); ++i) {
+			data[i] = static_cast<char>(toupper (data[i]));
+		}
+		return data;
+	}
+}
---
+++ src/Utilities.hpp
@@ -0,0 +1,16 @@
+#ifndef UTILITIES_HPP
+#define UTILITIES_HPP
+#include <string>
+
+namespace Project2_Utilites {
+	void clearScreen ( );
+
+	//Converts a string to it's lowercase equivalent.
+	std::string upperConvert (std::string data);
+
+	//Return's true if the user enters 'y' or 'Y' and false if 'N' or 'n'.
+	bool getYesOrNo ( );
+
+	int getValidInt ( );
+}
+#endif
