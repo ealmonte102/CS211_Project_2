9f4f6d05097b96b0b1f079385bd45352672bcba2
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-13 19:28:27 -0500

    Adds new class FileCollection
    -	Adds declarations for various member functions:
    	1)	Default constructor
    	2)	mainMenu
    	3)	readFromFile
    	4)	createImageFile
    	5)	createTextFile
    	6)	printAllFiles
    	7)	printFiles
    	8)	deleteFile
    	9)	findFile
    -	Adds an enum called findFlag.

---
+++ src/FileCollection.hpp
@@ -0,0 +1,17 @@
+﻿#ifndef FILECOLLECTION_HPP
+#define FILECOLLECTION_HPP
+
+#include "File.hpp"
+
+#include <vector>
+
+class FileCollection {
+public:
+	FileCollection ( );
+
+private:
+	std::vector<File*
+};
+
+#endif //FILECOLLECTION_HPP
+
